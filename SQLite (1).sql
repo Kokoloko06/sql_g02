3154e1b1be1e75760be2845e230b841aaf0f5ec1
Kokoloko06 <153167734+Kokoloko06@users.noreply.github.com>
2024-04-10 17:00:55 +0200

    Add files via upload

---
+++ SQLite (1).sql
@@ -0,0 +1,29 @@
+/*1. Készítsünk nézetet VSZOBA néven, amely megjeleníti a szobák adatai mellett a megfelelő szálláshely nevét, helyét és a csillagok számát is!
+
+Az oszlopoknak nem szükséges külön nevet adni!
+Teszteljük is a nézetet, pl: SELECT * FROM UJAENB_VSZOBA*/
+
+
+/*2 Készítsen tárolt eljárást SPUgyfelFoglalasok, amely a paraméterként megkapott ügyfél azonosítóhoz tartozó foglalások adatait listázza!
+Teszteljük a tárolt eljárás működését, pl: EXEC UJAENB_SPUgyfelFoglalasok 'laszlo2'
+*/
+
+/*
+3. Készítsen skalár értékű függvényt UDFFerohely néven, amely visszaadja, hogy a paraméterként megkapott foglalás azonosítóhoz hány férőhelyes szoba tartozik!
+a. Teszteljük a függvény működését!
+*/
+
+CREATE OR ALTER FUNCTION UDFFerohely
+(
+  	@foglalasazon INT
+  )
+  returns INT 
+  AS
+  BEGIN
+  	DECLARE @f int
+  	SELECT @f = sz.FEROHELY
+    FROM FOGLALAS F JOIN SZOBA SZ ON F.SZOBA_FK = SZ.SZOBA_ID
+    WHERE f.foglalas_pk = @foglalasazon
+ 	  return 
+  
+  END 
